96b2056da38f96f80963b627b774b2b3d99c70d5
Simon <ssmdmsn@hotmail.com>
2016-11-30 22:03:16 +0800

    build server

--- beginning/server.js
+++ beginning/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Simon\n');
+  res.end('Hello Myworld\n');
 });
 
 server.listen(port, hostname, () => {
